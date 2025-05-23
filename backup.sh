af1265a970f9b603c49e9554e169975071409af4
Stepan <rentv537@gmail.com>
2025-05-23 09:51:03 +0300

    add

--- backup.sh
+++ backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Begin";mkdir -p /opt/backup;tar -czf /opt/backup/$(hostname -s)-$(date +'%Y-%m-%d_%H:%M:%S').tgz /etc;echo "End"
+echo "Begin";mkdir -p /opt/backup;tar -czf /opt/backup/$(hostname -s)-$(date +'%y.%m.%d-%H:%M:%S').tgz /etc;echo "End"
