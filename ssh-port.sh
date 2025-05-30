7c76ecad910660edeb475270172d7a210501ba74
Stepan <rentv537@gmail.com>
2025-05-30 09:03:22 +0300

    or

--- ssh-port.sh
+++ ssh-port.sh
@@ -1,9 +1,9 @@
 
-iptables -t nat -A PREROUTING -p tcp --dport 2222 -d 192.168.100.2 -j DNAT --to-destination 192.168.100.2:22 && 
+iptables -t nat -A PREROUTING -p tcp --dport 2222 -d 192.168.100.2 -j DNAT --to-destination 192.168.100.2:22 && \
 
-iptables -A FORWARD -p tcp --dport 22 -d 192.168.100.2 -m conntrack ! --ctstate DNAT -j DROP && 
-iptables -S &&
-iptables -t nat -S && 
+iptables -A FORWARD -p tcp --dport 22 -d 192.168.100.2 -m conntrack ! --ctstate DNAT -j DROP && \
+iptables -S && \
+iptables -t nat -S && \
 
-systemctl enable --now iptables && 
+systemctl enable --now iptables && \
 iptables-save > /etc/sysconfig/iptables 
