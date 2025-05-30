bf9341907dac54435a656da2f438a2b3c5df2b82
Stepan <rentv537@gmail.com>
2025-05-30 09:05:36 +0300

    or

--- ssh-port.sh
+++ ssh-port.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 iptables -t nat -A PREROUTING -p tcp --dport 2222 -d 192.168.100.2 -j DNAT --to-destination 192.168.100.2:22 && \
 
 iptables -A FORWARD -p tcp --dport 22 -d 192.168.100.2 -m conntrack ! --ctstate DNAT -j DROP && \
