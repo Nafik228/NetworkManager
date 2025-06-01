efb4ff2ddd5d2a53388504b479ff2630bfd3dfd7
Stepan <132485196+ataraxzz@users.noreply.github.com>
2025-06-01 10:49:19 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,3 +3,6 @@ echo "Begin";
 mkdir -p /backup/;
 tar -czf /backup/$(hostname -s)-$(date +'%y.%m.%d-%H.%M.%S').tgz -C /etc/NetworkManager system-connections;
 echo "End"
+
+# tar -tf - просмотр архива
+# tar -xvzf - распакоука
