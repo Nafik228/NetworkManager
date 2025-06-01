c6809ecda8672b749893e6b65f1efc8acd50213a
Stepan <132485196+ataraxzz@users.noreply.github.com>
2025-06-01 10:37:55 +0300

    Update isp-nat.sh

--- isp-nat.sh
+++ isp-nat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE && systemctl enable iptabes && iptables-save >/etc/sysconfig/iptables
+iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE && systemctl enable --now iptabes && iptables-save >/etc/sysconfig/iptables
