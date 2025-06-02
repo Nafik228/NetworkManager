deb9511d4167a27bd1eac5dcb570e6a94e7b1c8f
Stepan <132485196+ataraxzz@users.noreply.github.com>
2025-06-02 20:37:11 +0300

    Update ssh-port.sh

--- ssh-port.sh
+++ ssh-port.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -p tcp --dport 2222 -d 192.168.100.5 -j DNAT --to-destination 192.168.100.5:22 && \
-iptables -A FORWARD -p tcp --dport 22 -d 192.168.100.5 -m conntrack ! --ctstate DNAT -j DROP && \
-iptables -S && \
-iptables -t nat -S && \
-systemctl enable --now iptables && \
+iptables -t nat -A PREROUTING -p tcp --dport 2222 -d 192.168.100.5 -j DNAT --to-destination 192.168.100.5:22 
+iptables -A FORWARD -p tcp --dport 22 -d 192.168.100.5 -m conntrack ! --ctstate DNAT -j DROP 
+iptables -S 
+iptables -t nat -S 
+systemctl enable --now iptables 
 iptables-save >/etc/sysconfig/iptables 
