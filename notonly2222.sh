b7088e904cb51bc9fc50eeaf06f339e9b52a0333
Nafik228 <sbagmet228@mail.ru>
2025-06-02 22:49:21 +0300

    Update notonly2222.sh

--- notonly2222.sh
+++ notonly2222.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-iptables -t nat -A PREROUTING -p tcp --dport 2222 -i enp0s3 -j DNAT --to-destination 192.168.100.5:22
+iptables -t nat -A PREROUTING -p tcp --dport 2222 -i enp0s3 -j DNAT --to-destination 192.168.10.5:22
 systemctl enable --now iptables 
 iptables-save >/etc/sysconfig/iptables 
 reboot
